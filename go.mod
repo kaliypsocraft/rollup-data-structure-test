dcd86e7e8cf97e463e9225321da6ac18478cf816
kali <jmecraft@jamiekl>
2024-10-16 16:28:09 +1100

    first commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/kaliypsocraft/rollup-data-structure-test
+
+go 1.23.2
+
+require (
+	github.com/alinush/go-mcl v0.0.0-20210224202455-eb6000c9b115 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/hyperproofs/gipa-go v0.0.0-20220302191226-79b6b6b9d404 // indirect
+	github.com/hyperproofs/hyperproofs-go v0.0.0-20230926225939-64c8fa5131d2 // indirect
+	github.com/hyperproofs/kzg-go v0.0.0-20220302191111-5aea771ec346 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+    "github.com/alinush/go-mcl"
+    "github.com/hyperproofs/hyperproofs-go/vcs"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	testing.Init()
+	flag.Parse()
+
+	fmt.Println("Hello, World!")
+	mcl.InitFromString("bls12-381")
+
+	dt := time.Now()
+	fmt.Println("Specific date and time is: ", dt.Format(time.UnixDate))
+
+	fmt.Println(vcs.SEP)
+
+	args := os.Args
+
+	if len(args) == 1 {
+		var L uint8
+		L = uint8(26)
+		//_ = hyperGenerateKeys(L, false)
+
+		L = uint8(30)
+		//_ = hyperGenerateKeys(L, true)
+	} else {
+		if args[1] == "1" {
+			// snarks_verifier()
+            fmt.Println("snark_verifier()")
+		} else {
+			fmt.Println("Benchmark()")
+		}
+	}
+}
+
+func hyperGenerateKeys(L uint8, fake bool) *vcs.VCS {
+
+	N := uint64(1) << L
+	vcs := vcs.VCS{}
+
+	fmt.Println("L:", L, "N:", N)
+	folderPath := fmt.Sprintf("pkvk-%02d", L)
+
+	if fake {
+		vcs.KeyGenFake(16, L, folderPath, 1<<12)
+        } else {
+            vcs.KeyGen(16, L, folderPath, 1<<12)
+        }
+        
+	fmt.Println("KeyGen ... Done")
+	return &vcs
+}
+
+func hyperLoadKeys(L uint8) *vcs.VCS {
+
+	folderPath := fmt.Sprintf("pkvk-%02d", L)
+	vcs := vcs.VCS{}
+
+	vcs.KeyGenLoad(16, L, folderPath, 1<<12)
+
+	fmt.Println("KeyGenLoad ... Done")
+	return &vcs
 }
